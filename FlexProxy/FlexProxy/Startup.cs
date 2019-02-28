b5a316a2c72fac426a255a94abb58b1adda66041
Ke Tang <jiushizhutk@gmail.com>
2019-02-27 21:04:52 -0500

    added configuration and updated launch settings for debugging

--- FlexProxy/FlexProxy/Startup.cs
+++ FlexProxy/FlexProxy/Startup.cs
@@ -8,22 +8,45 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace FlexProxy
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        private readonly ILogger _logger;
+        private readonly ILoggerFactory _loggerFactory;
+        private IServiceProvider _serviceProvider;
+        private string[] _args;
 
-        public IConfiguration Configuration { get; }
+        static public IConfigurationRoot Configuration { get; set; }
+
+        public Startup(IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            _loggerFactory = loggerFactory;
+            loggerFactory.AddConsole();
+            _logger = loggerFactory.CreateLogger<Startup>();
+            _args = Environment.GetCommandLineArgs().Skip(1).ToArray();
+            _logger.LogDebug($"Startup args: {string.Join(" ", _args)}");
+
+            var builder = new ConfigurationBuilder()
+                .AddCommandLine(_args)
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile($"appsettings.json")
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true);
+
+            Configuration = builder.Build();
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            
+            ConfigureOptions(services);
+        }
+
+        internal void ConfigureOptions(IServiceCollection services)
+        {
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +60,11 @@ namespace FlexProxy
             {
                 app.UseExceptionHandler("/Error");
             }
+
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("Test Response");
+            });
         }
     }
 }

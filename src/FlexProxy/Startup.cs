a64554ecbccb8b12009d71e8337d67fe12ef48f9
Ke Tang <jiushizhutk@gmail.com>
2019-03-04 20:56:31 -0500

    Updated docker file. Updated configurations for options.

--- src/FlexProxy/Startup.cs
+++ src/FlexProxy/Startup.cs
@@ -1,16 +1,22 @@
 using System;
 using System.Collections.Generic;
+using System.IO.Compression;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using FlexProxy.Core;
+using FlexProxy.Core.Models;
 using FlexProxy.ExceptionHandlerMiddleware;
 using FlexProxy.HealthCheckMiddleware;
 using FlexProxy.RequestTracerMiddleware;
 using FlexProxy.RobotsMiddleware;
+using FlexProxy.SessionHandlerMiddleware;
 using FlexProxy.WebProxyMiddleware;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -50,7 +56,42 @@ namespace FlexProxy
 
         internal void ConfigureOptions(IServiceCollection services)
         {
+            services.Configure<GzipCompressionProviderOptions>(options =>
+            {
+                options.Level = CompressionLevel.Fastest;
+            });
 
+            services.Configure<ResponseCompressionOptions>(options =>
+            {
+                options.Providers.Add<GzipCompressionProvider>();
+                options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[] { "text/javascript" });
+            });
+
+            services.Configure<WebProxyOptions>(options =>
+            {
+                options.InternalCookies = new List<string> { Configuration["EventSessionCookieName"] };
+            });
+
+            services.Configure<SessionHandlerOptions>(options =>
+            {
+                options.EventSessionCookieName = Configuration["EventSessionCookieName"];
+            });
+
+            services.Configure<HealthCheckOptions>(options =>
+            {
+                options.Localhost = IPAddress.Parse("127.0.0.1");
+                options.MaxResponseTimeInSeconds = int.Parse(Configuration["MaxResponseTimeInSeconds"]);
+            });
+
+            var uri = new Uri(_args[0].Substring(_args[0].IndexOf('=') + 1));
+            var domain = uri.Host;
+
+            services.Configure<RequestTraceLoggerServiceOptions>(options =>
+            {
+                options.LoggerConfigName = Constants.REQUEST_TRACELOGGER_NAME;
+                options.Interval = Constants.REQUEST_TRACELOGGER_INTERVAL;
+                options.ProcessName = domain;
+            });
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -68,6 +109,8 @@ namespace FlexProxy
             app.UseRequestTracer();
             app.UseCustomExceptionHandler();
             app.UseRobots();
+            app.UseResponseCompression();
+            app.UseSessionHandler();
             app.UseWebProxy();
         }
     }
